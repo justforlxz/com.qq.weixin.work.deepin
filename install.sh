2f18507bb035f0492e105d3c5499c57b8a12aad6
justforlxz <justforlxz@gmail.com>
2021-09-17 08:38:12 +0800

    update

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 echo "Start installing..."
 
-for _deb in $(ls *.deb); do
-    echo "Installing $_deb"
-    ar p "$_deb" data.tar.xz | tar -xJ
-done
-
-cp -a opt/apps/com.qq.weixin.work.deepin/* /app/
-
+unzip com.qq.weixin.work.deepin.zip -d tmp
 mkdir /app/bin
-cp run.sh /app/bin
-chmod a+x /app/bin/run.sh
+mv tmp/com.qq.weixin.work.deepin/run.sh /app/bin
+#chmod a+x /app/bin/run.sh
+cp -r tmp/com.qq.weixin.work.deepin/* /app/
 
 echo "Done"
