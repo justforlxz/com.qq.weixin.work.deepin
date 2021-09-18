38c928c035e6f2057fae2c3fcc5ec7aabf9ba152
justforlxz <justforlxz@gmail.com>
2021-09-18 14:24:33 +0800

    update

--- install.sh
+++
@@ -1,9 +0,0 @@
-echo "Start installing..."
-
-unzip com.qq.weixin.work.deepin.zip -d tmp
-mkdir /app/bin
-mv tmp/com.qq.weixin.work.deepin/run.sh /app/bin
-#chmod a+x /app/bin/run.sh
-cp -r tmp/com.qq.weixin.work.deepin/* /app/
-
-echo "Done"
